234dab745879a1c9a056cfaa9007b49c235ab72b
fundacion123 <165667672+fundacion123@users.noreply.github.com>
2024-04-01 23:58:30 +0200

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -43,6 +43,8 @@ function Login() {
         <input className='input' placeholder='Your email' type='email' name='email' onChange={handleChange} value={form.email} />
         <input className='input' placeholder='Your password' type='password' name='password' onChange={handleChange} value={form.password} />
         <button className='form-button'>Log In</button>
+        {/* Botón adicional "Go to Dapp" */}
+        <a href="http://www.ecode.es/" target="_blank" rel="noopener noreferrer" className='form-button' style={{marginTop: '10px'}}>Go to Dapp</a>
         <Link to="/register">Not registered? Click here</Link>
       </form>
     </div>
